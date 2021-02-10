16994f1cc8817c6c32806589baacb22fba5f9e2d
Mike Martin <mtmgoliath@gmail.com>
2021-02-10 14:43:15 +0000

    have reworked while loop, removing echos and interspersed php/html and db->queries from function, into a foreach loop. However it now only makes the first result of the array appear. TODO array-count to set length of loop

---
+++ functions.php
@@ -0,0 +1,50 @@
+<?php
+$db = new PDO('mysql:host=db; dbname=collector_project', 'root', 'password');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$fullQuery = $db->prepare("SELECT * FROM `taxidermy_collection`;");
+$fullQuery->execute();
+$results = $fullQuery->fetchAll();
+var_dump(count($results));
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="en-GB">
+    <head>
+        <title>Taxidermy Collection</title>
+        <link rel="stylesheet" type="text/css" href="normalize.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+    <header>
+        <h1>Life in Death</h1>
+        <h2>Curios & Oddities</h2>
+    </header>
+    <section id="intro">
+        <p>Here is a nice gallery of my Taxidermy Collection so far.
+            Items are sorted by their Name/ID and include details about their origin,
+            as well as an overall rating out of 10.</p>
+    </section>
+    <ul class="collection">
+        <? echo populateItems($results) ?>
+    </ul>
+    </body>
+    </html>
+
+
+<?php
+
+function populateItems($results) {
+    foreach ($results as $row) {
+        $collectionString = '<li class="collection_item "><div class="stats"><span class="image_wrapper">';
+        $collectionString .= '<img src="' . $row['image'] . '"';
+        $collectionString .= ' alt="' . $row['description'] . '"></span>';
+        $collectionString .= '<h3>' . $row['name'] . '</h3>';
+        $collectionString .= '<ul><li class="origin">Origin:' . $row['origin'] . '</li>';
+        $collectionString .= '<li class="family">Family:' . $row['family'] . '</li>';
+        $collectionString .= '<li class="preservation_type">' . $row['method'] . '</li>';
+        $collectionString .= '<li class="quality">Rating:' . $row['rating'] . '/10</li>' . '</ul></div></li>';
+        return $collectionString;
+    }
+}
