a99e6f3655abe427ee2d40db5a33a3a4d64d95fe
Mike Martin <mtmgoliath@gmail.com>
2021-02-11 16:35:16 +0000

    styling for form and link from index

--- upload-form.php
+++ upload-form.php
@@ -22,22 +22,39 @@ if (isset($_POST['item-name'])) {
 </section>
 <div class="form-container">
     <form action="data-items.php" method="post" name="upload-form">
-        <label for="item-name">Name</label>
-        <input name="item-name" type="text" placeholder="i.e. Red Colobus"/>
-        <label for="item-origin">Country of Origin</label>
-        <input name="item-origin" type="text" placeholder="i.e. Zanzibar"/>
-        <label for="item-family">Family</label>
-        <input name="item-family" type="text" placeholder="i.e. Mammal"/>
-        <label for="item-method">Preservation Method</label>
-        <input name="item-method" type="text" placeholder="i.e. Wet Specimen"/>
-        <label for="item-rating">Rating /10:</label>
-        <input name="item-rating" type="number" placeholder="enter a number from 1-10" min="1" max="10"/>
-        <label for="item-image">Image URL:</label>
-        <input name="item-image" type="text" placeholder="250 characters max"/>
-        <label for="item-description">Brief description:</label>
-        <textarea name="item-description"  placeholder="250 characters max" maxlength="250"></textarea>
+        <div class="form-row">
+            <label for="item-name">Name</label>
+            <input name="item-name" type="text" placeholder="i.e. Red Colobus"/>
+        </div>
+        <div class="form-row">
+            <label for="item-origin">Country of Origin</label>
+            <input name="item-origin" type="text" placeholder="i.e. Zanzibar"/>
+        </div>
+        <div class="form-row">
+            <label for="item-family">Family</label>
+            <input name="item-family" type="text" placeholder="i.e. Mammal"/>
+        </div>
+        <div class="form-row">
+            <label for="item-method">Preservation Method</label>
+            <input name="item-method" type="text" placeholder="i.e. Wet Specimen"/>
+        </div>
+        <div class="form-row">
+            <label for="item-rating">Rating /10:</label>
+            <input name="item-rating" type="number" placeholder="enter a number from 1-10" min="1" max="10"/>
+        </div>
+        <div class="form-row">
+            <label for="item-image">Image URL:</label>
+            <input name="item-image" type="text" placeholder="250 characters max"/>
+        </div>
+        <div class="form-row">
+            <label for="item-description">Brief description:</label>
+            <textarea name="item-description"  placeholder="250 characters max" maxlength="250"></textarea>
+        </div>
         <input name="submit" type="submit">
     </form>
+    <nav>
+        <a href="index.php">Return to collection</a>
+    </nav>
 </div>
 </body>
 </html>
