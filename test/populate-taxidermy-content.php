764e564e4358ddcf6e88ff5642e2d5d9fd08cebf
mtmgoliath <70155580+mtmgoliath@users.noreply.github.com>
2021-02-11 11:34:03 +0000

    Update populate-taxidermy-content.php

--- test/populate-taxidermy-content.php
+++ test/populate-taxidermy-content.php
@@ -2,18 +2,6 @@
 require_once '../populate-taxidermy-content.php';
 use PHPUnit\Framework\TestCase;
 
-
-
-$mockRowOutput = '';
-$mockRowOutput .= '<li class="collection_item "><div class="stats"><span class="image_wrapper">';
-$mockRowOutput .= '<img src="one"';
-$mockRowOutput .= ' alt="two"></span>';
-$mockRowOutput .= '<h3>test</h3>';
-$mockRowOutput .= '<ul><li class="origin">Origin: three</li>';
-$mockRowOutput .= '<li class="family">Family: four</li>';
-$mockRowOutput .= '<li class="preservation_type">five</li>';
-$mockRowOutput .= '<li class="quality">Rating: six/10</li></ul></div></li>';
-
 class PopulateTaxidermyContent extends TestCase {
 
     //Mock functions for input into tests
